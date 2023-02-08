235240beed1f4c4bd8991fd0e5189a67f6ba4553
riandkbayui <riandkbayui@gmail.com>
2023-02-08 10:55:48 +0700

    modify vercel json

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.get("/", (req, res) => {
 app.get("/download", async(req, res) => {
 	const v_id = req.query.url.split('v=')[1];
     const info = await ytdl.getInfo(req.query.url);
-    console.log(info.formats[4]);
-    console.log(info.formats[1]);
 
 	return res.render("download", {
 		url: "https://www.youtube.com/embed/" + v_id,
